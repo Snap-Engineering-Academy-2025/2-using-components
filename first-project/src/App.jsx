537d7871ad396822d7fde4d9a876db90fa744767
ajjeroni <adanjeronimo213@gmail.com>
2025-06-26 15:53:56 -0700

    completed third part - search component

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,13 +1,21 @@
-
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import "./App.css";
-
 import CardGrid from "./cardGrid";
 import teas from "./teas.json";
 import CustomButton from "./CustomButton";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import React from "react";
 
 function App() {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  console.log("searchTerm:", searchTerm);
+
+  let filteredTeas = teas.filter((tea) =>
+    tea.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <>
       <Container maxWidth="md" sx={{ mb: 4 }}>
@@ -28,10 +36,27 @@ function App() {
           Why isn't the CSS working?
         </Typography>
       </Container>
-      
+
+      <Box
+        component="form"
+        sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="outlined-controlled"
+          label="Search for a tea"
+          value={searchTerm}
+          onChange={(event) => {
+            console.log("Event:", event.target.value);
+            setSearchTerm(event.target.value);
+          }}
+        />
+      </Box>
+
       <CustomButton />
 
-      <CardGrid jsondata={teas}/>
+      <CardGrid jsondata={filteredTeas} />
     </>
   );
 }
